db19233b0945046f81157716857908cbed6fd93e
hafiz4m1 <133829025+hafiz4m1@users.noreply.github.com>
2023-05-23 13:57:55 +0800

    upload pertama

---
+++ Unit7.dfm
@@ -0,0 +1,40 @@
+object Form7: TForm7
+  Left = 292
+  Top = 190
+  Width = 1044
+  Height = 541
+  Caption = 'Form7'
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  Menu = menu
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object menu: TMainMenu
+    Left = 24
+    Top = 8
+    object menu1: TMenuItem
+      Caption = 'menu'
+      object kalkulator1: TMenuItem
+        Caption = 'kalkulator'
+        OnClick = kalkulator1Click
+      end
+      object datagrafik1: TMenuItem
+        Caption = 'data grafik'
+        OnClick = datagrafik1Click
+      end
+      object latihan21: TMenuItem
+        Caption = 'latihan 2'
+        OnClick = latihan21Click
+      end
+      object grafikinformasi1: TMenuItem
+        Caption = 'grafik informasi'
+        OnClick = grafikinformasi1Click
+      end
+    end
+  end
+end
---
+++ Unit7.pas
@@ -0,0 +1,57 @@
+unit Unit7;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, Menus;
+
+type
+  TForm7 = class(TForm)
+    menu: TMainMenu;
+    menu1: TMenuItem;
+    kalkulator1: TMenuItem;
+    datagrafik1: TMenuItem;
+    latihan21: TMenuItem;
+    grafikinformasi1: TMenuItem;
+    procedure kalkulator1Click(Sender: TObject);
+    procedure datagrafik1Click(Sender: TObject);
+    procedure latihan21Click(Sender: TObject);
+    procedure grafikinformasi1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form7: TForm7;
+
+implementation
+
+uses
+  Unit1, Unit2, Unit3, Unit4;
+
+{$R *.dfm}
+
+procedure TForm7.kalkulator1Click(Sender: TObject);
+begin
+  Form1.show;
+end;
+
+procedure TForm7.datagrafik1Click(Sender: TObject);
+begin
+ Form2.Show;
+end;
+
+procedure TForm7.latihan21Click(Sender: TObject);
+begin
+  Form3.Show;
+end;
+
+procedure TForm7.grafikinformasi1Click(Sender: TObject);
+begin
+ Form4.Show;
+end;
+
+end.
